c546c8ac3a5f5c23781ae8aacd07638aa4a98600
Najoua Bentaher <117578236+NajouaBentaher@users.noreply.github.com>
2023-08-29 10:06:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-let resultsContainer = document.getElementsByClassName("container")[0]
+let resultsContainer = document.getElementsByClassName("container")[0];
+let debounceTimeout;
 
 const validateInput = (el) => {
-    if(el.value === ""){
-        resultsContainer.innerHTML = "<p>Type something in the above search input</p>"
-    }else {
+    if (el.value === "") {
+        resultsContainer.innerHTML = "<p>Type something in the above search input</p>";
+    } else {
+        
+        
         clearTimeout(debounceTimeout); 
         debounceTimeout = setTimeout(() => {
             generateResults(el.value, el);
@@ -18,23 +21,23 @@ const generateResults = (searchValue, inputField) => {
     )
     .then(response => response.json())
     .then(data => {
-        let results = data.query.search
-        let numberOfResults = data.query.search.length
-        resultsContainer.innerHTML = ""
-        for(let i=0; i<numberOfResults; i++) {
-            let result = document.createElement("div")
-            result.classList.add("results")
+        let results = data.query.search;
+        let numberOfResults = data.query.search.length;
+        resultsContainer.innerHTML = "";
+        for (let i = 0; i < numberOfResults; i++) {
+            let result = document.createElement("div");
+            result.classList.add("results");
             result.innerHTML = `
             <div>
                 <h3>${results[i].title}</h3>
                 <p>${results[i].snippet}</p>
             </div>
             <a href="https://en.wikipedia.org/?curid=${results[i].pageid}" target="_blank">Read More</a>
-            `
-            resultsContainer.appendChild(result)
+            `;
+            resultsContainer.appendChild(result);
         }
-        if(inputField.value === ""){
-            resultsContainer.innerHTML = "<p>Type something in the above search input</p>"
+        if (inputField.value === "") {
+            resultsContainer.innerHTML = "<p>Type something in the above search input</p>";
         }
     })
 }
